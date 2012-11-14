f293acf72cae9caaa792c310cfc0dcd9f166df03
Nolan Lum <nol888@gmail.com>
2012-11-14 01:01:17 -0800

    Improve Windows build system. (requires sed)

--- make.bat
+++ make.bat
@@ -1,5 +1,7 @@
 @if not exist org mkdir org
 @cd src
-@dir /s /b *.java > ../org/srcfiles.txt
+@dir /s /b *.java > ../org/files.txt
 @cd ..
+@sed -r -e "s/.*/\"\0\"/" -e "s~\\~/~g" org/files.txt > org/srcfiles.txt
+@del org\files.txt
 javac -d . -classpath sqlite-jdbc-3.7.2 @org/srcfiles.txt
